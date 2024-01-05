de844d24efea2f360ec6079970234cee6524fa2a
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-05 11:53:06 +0300

    add 5-no_c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    string = ""
+    for i in my_string:
+        if i == "c" or i == "C":
+            continue
+        else:
+            string += i
+    return str(string)
