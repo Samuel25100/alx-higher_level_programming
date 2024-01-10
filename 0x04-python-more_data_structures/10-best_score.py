c81a9319fe2badc82e6fbfd60c39ca27c1414750
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-10 16:56:14 +0300

    fixing file 10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    value = 0
     if a_dictionary is None:
         return None
-    value = max(a_dictionary, key=a_dictionary.get)
-    return value
+    return max(a_dictionary, key=a_dictionary.get)
