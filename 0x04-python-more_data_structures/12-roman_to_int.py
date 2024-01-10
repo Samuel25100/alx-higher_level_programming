9e48bf4f95ed46c462cde4eafadb8176e3b949ce
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-10 16:10:15 +0300

    add 12-roman_to_int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num_list = []
+    for i in roman_string:
+        num_list.append(data[i])
+    num_list += [0]
+    value = 0
+    for i in range(len(num_list) - 1):
+        if num_list[i] >= num_list[i + 1]:
+            value += num_list[i]
+        else:
+            value -= num_list[i]
+    return value
