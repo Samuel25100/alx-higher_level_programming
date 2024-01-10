fdeffb7994205fc92c2f21ce101d7bc7537a5eca
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-10 16:23:33 +0300

    fixing 12-roman...

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 def roman_to_int(roman_string):
-    if type(roman_string) is not str:
+    if (type(roman_string) is not str) or roman_string is None:
         return 0
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_list = []
