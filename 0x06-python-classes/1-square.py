386386ac632a6951da0dd018f90628682b6bb25a
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-23 13:18:35 +0300

    add 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A module for class Square"""
+
+
+class Square:
+    """Square is a class with attributes of size"""
+    def __init__(self, size):
+        self.__size = size
