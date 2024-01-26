79fe5abf36f76d855adf83b3c5494d7be1ca5f0a
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-26 13:10:48 +0300

    add 2-matrix_divided py and txt

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Divide the argument matrix to div return new matrix"""
+
+
+def matrix_divided(matrix, div):
+    if (not isinstance(matrix, list) and
+            not all(isinstance(i, list) for i in matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:
+        for j in i:
+            if not isinstance(j, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if (not all(len(matrix[0]) == len(i) for i in matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_mat = [[j/div for j in i]for i in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_mat[i][j] = round(new_mat[i][j], 2)
+    return (new_mat)
