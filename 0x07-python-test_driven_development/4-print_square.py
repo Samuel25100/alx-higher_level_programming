7358a8e9277a77829bf7bc423562cb3bfe4ce4c6
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-27 09:18:02 +0300

    add 4-print_square py and txt

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Print square - print square using char '#' in size argument size"""
+
+
+def print_square(size):
+    if ((not isinstance(size, int)) or
+            (isinstance(size, float) and (size < 0))):
+        raise TypeError("size must be an integer")
+    if not (size >= 0):
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
