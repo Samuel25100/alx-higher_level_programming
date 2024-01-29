62dee852f305c7e530cb6a2a54ca6892cbda0ed4
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-29 17:30:00 +0300

    fixing documentation

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""empty class Rectangle that defines a rectangle"""
+"""empty class Rectangle that defines a rectangle."""
 
 
 class Rectangle:
+    """Is the class of rectangle using char."""
+
     pass
