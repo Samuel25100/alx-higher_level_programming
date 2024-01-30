b952734a1ae1158f0997d980bd35126b273e0b7d
Samuel25100 <samuelgselassie1@gmail.com>
2024-01-30 15:25:07 +0300

    add 101-lazy_matrix_mul py and txt

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Matrix multiplication using numpy module."""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Multiply m_a and m_b matrix using numpy."""
+    return numpy.matmul(m_a, m_b)
