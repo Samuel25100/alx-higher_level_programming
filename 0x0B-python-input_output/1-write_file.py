0190097a654200fc802e5ba7c8545d2090008a11
Samuel25100 <samuelgselassie1@gmail.com>
2024-02-06 13:14:41 +0300

    fixing pydoc issue

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""write a string to a text file (UTF8), returns the num of char"""
+"""Write a string to a text file (UTF8), returns the num of char."""
 
 
 def write_file(filename="", text=""):
+    """Write on file 'filename' the string 'text'."""
     with open(filename, 'w', encoding="UTF8") as f:
         num = f.write(text)
     return (num)
