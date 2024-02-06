3d977fc6b80a266694a26aebf7a2c269d990887a
Samuel25100 <samuelgselassie1@gmail.com>
2024-02-06 16:48:22 +0300

    fixing 9-student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """Define class Student."""
+
+
 class Student:
+    """Repersent class Student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initalize attribute."""
+        """Init initalize attribute."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
