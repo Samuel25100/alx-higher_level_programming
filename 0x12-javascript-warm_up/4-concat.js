d184b07dd29fd0f7033509a0d287c04ee4dcf3b7
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-08 14:13:55 +0300

    fixing 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* prints a message depending of the number of arguments passed */
 if (process.argv.length <= 2) {
-  console.log('undefine is undefine');
+  console.log('undefined is undefined');
 } else if (process.argv.length === 3) {
-  console.log(process.argv[2], ' is undefine');
+  console.log(process.argv[2], ' is undefined');
 } else if (process.argv.length >= 4) {
   console.log(process.argv[2], 'is', process.argv[3]);
 }
