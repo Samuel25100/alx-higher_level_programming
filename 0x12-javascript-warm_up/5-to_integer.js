0fbc042413571857c75f5ef884259bb976dc31c8
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-08 13:56:44 +0300

    add file 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* Change type of argument if allowed then print it */
+if (process.argv.length <= 2) {
+  console.log('Not a number');
+} else if (!isNaN(process.argv[2])) {
+  console.log('My number:', process.argv[2]);
+} else {
+  console.log('Not a number');
+}
