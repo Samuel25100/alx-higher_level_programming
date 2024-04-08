fe110aa0345f4f55fc0827d08cd2cdd2c31e66a3
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-08 15:44:10 +0300

    add file 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* prints square x time  */
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else if (!isNaN(process.argv[2])) {
   let y = Number(process.argv[2]);
