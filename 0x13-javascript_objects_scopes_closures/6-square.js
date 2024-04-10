e8bfb664bbeb165798431c6a77936a430098604c
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-10 11:04:16 +0300

    add file 6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 /* define class square */
-const Rectangle = require('./4-rectangle');
+const Squ = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Squ {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
