32d69966c0ca0f62ba5bf9f86addfc2ad2c12df1
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-11 10:06:03 +0300

    add file README, 0,1,2 tasks

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa."""
+import MySQLdb as mysql
+import sys
+
+connection = mysql.connect(
+        host='localhost',
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+)
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+connection.close()
