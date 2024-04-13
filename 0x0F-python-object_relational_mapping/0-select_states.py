9d394293d1a4e96252ba0d8fd29618018083f9fb
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-13 07:56:29 +0300

    add file 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,18 @@
 """List all states from the database hbtn_0e_0_usa."""
 import MySQLdb as mysql
 import sys
-
-connection = mysql.connect(
-        host='localhost',
-        user=sys.argv[1],
-        password=sys.argv[2],
-        database=sys.argv[3]
-)
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM states")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-cursor.close()
-connection.close()
+if __name__ == "__main__":
+    connection = mysql.connect(
+            host='localhost',
+            user=sys.argv[1],
+            password=sys.argv[2],
+            database=sys.argv[3],
+            port=3306
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
