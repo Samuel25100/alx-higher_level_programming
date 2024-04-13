5a62ce7f7a7885cb12063ec81fa9437e6eab2cbf
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-13 19:12:53 +0300

    add file 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define class State which inherit from Base."""
+from sqlalchemy import create_engine, MetaData, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).filter(State.id == 2)
+    row[0].name = 'New Mexico'
+    session.commit()
