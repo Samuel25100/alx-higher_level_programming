c956c2e4b4cb7459920cf9781f9feab107f93d73
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-13 18:41:06 +0300

    add file 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define class State which inherit from Base."""
+from sqlalchemy import create_engine, MetaData, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    col = session.query(State).filter(State.name == (sys.argv[4],)).first()
+    if (col is None):
+        print("Not found")
+    else:
+        print(col.id)
