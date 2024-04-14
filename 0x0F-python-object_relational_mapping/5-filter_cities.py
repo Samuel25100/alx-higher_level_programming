13faffdd96f0362ddbd97997799b7e285d7d0534
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-14 06:47:16 +0300

    add file 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
         port=3306
     )
     cursor = connection.cursor()
-    cursor.execute("""SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') AS list
-    FROM cities INNER JOIN states ON states.id=cities.state_id
-    WHERE states.name=%s ORDER BY cities.id ASC""", (sys.argv[4],))
-    rows = cursor.fetchone()[0]
-    print(rows)
+    cursor.execute("""SELECT cities.name FROM cities INNER JOIN states
+    ON states.id=cities.state_id WHERE states.name=%s
+    ORDER BY cities.id ASC""", (sys.argv[4],))
+    rows = cursor.fetchall()
+    row = list(i[0] for i in rows)
+    print(*row, sep=', ')
     cursor.close()
     connection.close()
