8cb8a684eab2ac839e1a3ca0f4a5e023a4c6f350
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-25 15:35:35 +0300

    add file 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 # Take URL as argument and request to that URL and displays the size of the body
 
-size=$(curl -s -w "%{size_download}" $1)
-echo $size
+size=$(curl -s -w "%{size_download}" "$1")
+echo "$size"
