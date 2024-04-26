07f79cdb5143ff3539e4ea5b8a6a49cd7b3430f1
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-26 09:20:08 +0300

    add file 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fetch data from a url and handle error."""
+import urllib.request
+import sys
+
+
+def fetch_url():
+    """Fetch id value of X-request-Id from header."""
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            file = response.read()
+            print(file.decode('utf-8'))
+    except urllib.error.HTTPError as er:
+        print("Error code: ", er.code)
+
+
+if __name__ == "__main__":
+    fetch_url()
