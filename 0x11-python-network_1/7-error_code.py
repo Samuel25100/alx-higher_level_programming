2ed15286bf779d7b8d1796e98ff8face02a28688
Samuel25100 <samuelgselassie1@gmail.com>
2024-04-26 10:41:48 +0300

    add file 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Fetch data from a url and handle error."""
+import requests
+import sys
+
+
+def fetch_url():
+    """Fetch id value of X-request-Id from header."""
+    file = request.get(sys.argv[1])
+    if (file.status_code >= 400):
+        print("Error code:", file.status_code)
+
+
+if __name__ == "__main__":
+    fetch_url()
