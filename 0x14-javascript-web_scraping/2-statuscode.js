024c808d00f6710a2a2d4cd01d25a7a6f12c1767
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-21 15:42:19 +0300

    add file 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], (err, res) => {
+request(process.argv[2], function (err, res) {
   if (err == null) {
     console.log('code: ', res.statusCode);
   }
